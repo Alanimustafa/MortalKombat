8b6f2ac688fb1b761f06edaddf1cb6b49c8ec0c1
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-01-25 20:44:59 -0600

    Hoempage |  Background Video

--- TypeScript/homePage.ts
+++ TypeScript/homePage.ts
@@ -1,5 +1,25 @@
 // TypeScript file for  homePage.JS
 
+// Adding the background Video
+const mkBackground = document.createElement('video');
+    mkBackground.setAttribute('id', 'mkBackground')
+    // Setting up the vidoe attributes
+    mkBackground.setAttribute("autoplay", true);
+    mkBackground.setAttribute("muted", true);
+    mkBackground.setAttribute("loop", true);
+    mkBackground.setAttribute("playsinline", true);
+
+    // Add the source element
+    const source = document.createElement("source");
+    source.setAttribute("src", "https://cdn-mk1.mortalkombat.com/home/hero.mp4"); // Replace with your video URL
+    source.setAttribute("type", "video/mp4");
+
+    // Append the source to the video
+    mkBackground.appendChild(source);
+
+    // Append the Video in the body of the home page
+    document.body.appendChild(mkBackground);
+
 // Home Page | Navegation Bar | TOP
 const homePageMainNavBar = document.createElement('div');
 homePageMainNavBar.classList.add('homePageMainNavBar');
